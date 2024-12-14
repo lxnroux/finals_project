18f40babda0ec472a6973865f11894899800ab00
felix <felixcruz8166@gmail.com>
2024-12-15 00:02:48 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,23 @@
+fullname = "Fender B. Cruz"
+birthplace = "Lucena City"
+address = "Purok Maligaya Gulang Gulang Lucena City"
+birthmonth = "August"
+birthdate = 16
+birthyear = 2006
+gender = "Male"
+nationality = "Filipino"
+strand = "STE and STEM"
+oldschool = "Gulang Gulang National High School"
+grade = 94.688
+school = "Dalubhasaan ng Lungsod ng Lucena" 
+year = "First Year"
+course = "Bachelor of Science in Information Technology"
+email = "felixcruz8166@gmail.com"
+contactnumber = 639512446514
+single = True
+religion = "Catholic"
+hobbies = "art, like sketching, designing and fashion"
+
+
+
+print (fullname , "was born in" , birthplace , "and currently resides in" , address , "\nBorn" , birthmonth , birthdate , birthyear,". A" , gender , nationality , "student that took" , strand , "and graduated at" , oldschool , "with an average of" , grade , "with high honors \nNow a" , year , "student at" , school , "taking" , course,". For inquiries, his email is" , email , "and contact number is" , contactnumber , "\nIt is" , single , "that his marital status is single and he is" , religion , "\nMost of his interest revolves around" , hobbies  ) 
